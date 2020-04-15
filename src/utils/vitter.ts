16a94a74c4d0c39a006685ab03d71294630a1eee
PuerileSmuggler <tokarzewskis@gmail.com>
2020-04-15 22:45:10 +0200

    make it actually wokr

--- src/utils/vitter.ts
+++ src/utils/vitter.ts
@@ -14,6 +14,7 @@ class Node {
     public weight: number;
     public value: number;
     public letter: string | null = null;
+    public code = '';
 
     constructor(rightChild: nodeType, leftChild: nodeType, type: nodeTypes, weight: number, value: number, parent: nodeType, letter?: string) {
         this.rightChild = rightChild;
@@ -29,11 +30,20 @@ class Node {
 }
 
 class VitterTree {
-    public nodes: Node[] = [new Node(null, null, nodeTypes.NYT, 256, 0, null)];
+    public nodes: Node[];
+
+    public root: Node = new Node(null, null, nodeTypes.NYT, 256, 0, null);
 
     public inputArray: string[];
 
+    public code = '';
+
+    private block: Node[] = [];
+
+    private newNode: Node = new Node(null, null, nodeTypes.NYT, 256, 0, null);
+
     constructor(input: string) {
+        this.nodes = [this.root];
         this.inputArray = input.split('');
     }
 
@@ -52,41 +62,87 @@ class VitterTree {
     addLetter(letter: string): void {
         const node = this.isLetterInTree(letter);
         if(node) {
-            let parentNode = node.parent;
-            node.value++;
-            while (parentNode) {
-                parentNode.value = parentNode.value + 1;
-                console.log(letter, 'same', parentNode.value);
-                parentNode = parentNode.parent;
-            }
+            this.code += `${node.code} `;
+            this.newNode = node;
+            this.root.value++;
+            this.sortBlock();
         } else {
             const NYT = this.findNYT();
             if (NYT) {
-                this.nodes.push(new Node(null, null, nodeTypes.LEAF, NYT.weight - 1, 1, NYT, letter));
-                this.nodes.push(new Node(null, null, nodeTypes.NYT, NYT.weight - 2, 0, NYT));
+                this.code += `${NYT.code}0${letter.charCodeAt(0).toString(2)} `
+                const newNode = new Node(null, null, nodeTypes.LEAF, NYT.weight - 1, 1, NYT, letter);
+                const newNYT = new Node(null, null, nodeTypes.NYT, NYT.weight - 2, 0, NYT);
+                this.nodes.push(newNode);
+                this.nodes.push(newNYT);
+                NYT.rightChild = newNode;
+                NYT.leftChild = newNYT;
                 NYT.type = nodeTypes.INTERNAL;
-                NYT.value = 1;
-                let parentNode = NYT.parent;
-                while (parentNode) {
-                    parentNode.value++;
-                    console.log(letter, 'new', parentNode.value);
-                    parentNode = parentNode.parent;
-                }
+                NYT.value++;
+                this.newNode = NYT;
+                this.sortBlock();
             }
         }
-        this.updateTree();
 
+    }
+
+    private setBlock(): void {
+        if (this.newNode.leftChild && this.newNode.leftChild.type === nodeTypes.NYT) {
+            this.block = this.nodes.filter((node) => (node.value === this.newNode.value && node !== this.newNode.rightChild));
+        } else {
+            this.block = this.nodes.filter((node) => (node.value === this.newNode.value && node !== this.newNode));
+        }
+    }
+
+    private sortBlock(): void {
+        this.setBlock();
+        if (this.block.length) {
+            this.block.sort((a, b) => {
+                if (a.type === nodeTypes.INTERNAL || b.type === nodeTypes.INTERNAL) {
+                    return a.type === nodeTypes.INTERNAL ? 1 : -1;
+                }
+                return a.weight - b.weight;
+            });
+            if (this.newNode.leftChild && this.newNode.leftChild.type === nodeTypes.NYT) {
+
+               
+                this.block.forEach((blockNode, index) => {
+                    blockNode.weight = (this.newNode.leftChild as Node).weight + index + 2;
+                    console.log(blockNode.weight);
+                });
+                if (this.newNode !== this.root) {
+                    this.root.value++;
+                }
+            } else {
+                this.newNode.weight = this.block[this.block.length - 1].weight;
+                this.block.forEach((blockNode, index) => {
+                    blockNode.weight = this.newNode.weight - this.block.length + index;
+                });
+                this.newNode.value++;
+            }
+        } else {
+            this.newNode.value++;
+        }
+        let prevParent = this.newNode.parent;
+        this.updateTree();
+        let parent = this.newNode.parent;
+        while (parent) {
+            parent.value = parent.rightChild ? parent.rightChild.value : 0;
+            parent.value += parent.leftChild ? parent.leftChild.value : 0;
+            parent = parent.parent;
+        }
+
+        while (prevParent) {
+            prevParent.value = prevParent.rightChild ? prevParent.rightChild.value : 0;
+            prevParent.value += prevParent.leftChild ? prevParent.leftChild.value : 0;
+            prevParent = prevParent.parent;
+        }
+        
+        
     }
 
     private updateTree(): void {
         this.nodes.sort((a, b) => {
-            if ((a.type === nodeTypes.INTERNAL || b.type === nodeTypes.INTERNAL) && a.value === b.value ) {
-                return a.type === nodeTypes.INTERNAL ? 1 : -1;
-            }
-            if (a.value === b.value && (a.type !== nodeTypes.NYT || b.type !== nodeTypes.NYT)) {
-                return this.inputArray.findIndex((element) => element === b.letter) - this.inputArray.findIndex((element) => element === a.letter)
-            }
-            return a.value - b.value;
+            return a.weight - b.weight;
         });
         const nodesToAssign = [...this.nodes];
         nodesToAssign.pop();
@@ -96,17 +152,26 @@ class VitterTree {
                     const rightChild = nodesToAssign.pop();
                     if (rightChild) {
                         this.nodes[i].rightChild = rightChild;
-                        (this.nodes[i].rightChild as Node).parent = this.nodes[i];
+                        rightChild.parent = this.nodes[i];
+                        rightChild.code = `${rightChild.parent.code}${1}`
                     }
                     const leftChild = nodesToAssign.pop();
                     if (leftChild) {
                         this.nodes[i].leftChild = leftChild;
-                        (this.nodes[i].leftChild as Node).parent = this.nodes[i];
+                        leftChild.parent = this.nodes[i];
+                        leftChild.code = `${leftChild.parent.code}${0}`
                     }
                 }
             }
 
         }
+    }
+
+    private appendCode(node: Node, value: number): void {
+        if (node.parent && node.parent.code) {
+            node.code = node.parent.code + value.toString();
+        }
+        node.code = value.toString();
     }
 
     encode(): void {
@@ -124,6 +189,10 @@ function vitterEncode(input: string): void {
     tree.encode();
 
     console.log(tree.nodes);
+    console.log(tree.code);
+
+    //console.log(tree.root);
+    //console.log(tree.code);
 }
 
 export default vitterEncode;
